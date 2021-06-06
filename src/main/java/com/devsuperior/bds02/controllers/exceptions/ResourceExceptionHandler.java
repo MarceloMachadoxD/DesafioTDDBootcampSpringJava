fb08e65ac34872a836cb4f13184c6c63b6e2e862
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-06 12:12:59 -0300

    ExeptionHandler

---
+++ src/main/java/com/devsuperior/bds02/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.devsuperior.bds02.controllers.exceptions;
+
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        StandardError standardError = new StandardError();
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        standardError.setTimestamp(Instant.now());
+        standardError.setStatus(status.value());
+        standardError.setError("Resource Not found");
+        standardError.setMessage(e.getMessage());
+        standardError.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(standardError);
+
+
+    }
+
+
+
+}
