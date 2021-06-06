15051a05d9c74eabdd83ff943fdc5871e8271a17
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-06 12:44:23 -0300

    Adjusting location of CityController

--- src/main/java/com/devsuperior/bds02/controllers/CityController.java
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -1,4 +1,4 @@
-package com.devsuperior.bds02.controllers.exceptions;
+package com.devsuperior.bds02.controllers;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.services.CityService;
