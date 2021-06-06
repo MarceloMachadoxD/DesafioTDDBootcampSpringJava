c94ee30905cf67869ce24fe1162cce702a963143
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-06 12:31:37 -0300

    CityService FindAll Method

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -18,10 +18,10 @@ public class CityService {
     private CityRepository repository;
 
     @Transactional(readOnly = true)
-    public List<CityDTO> findAll(){
+    public List<CityDTO> findAll() {
         List<City> list = repository.findAll(Sort.by("name"));
 
         return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
-        }
+    }
 
 }
