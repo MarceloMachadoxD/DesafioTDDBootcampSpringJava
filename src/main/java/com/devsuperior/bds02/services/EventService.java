87e20adf3af31b0d15e232855ca8ac29ba0987c9
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-06 11:59:35 -0300

    Initial update EventService

---
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,37 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+//need to fix the throw exception
+    @Transactional(readOnly = false)
+    public EventDTO update(Long id, EventDTO dto){
+        try {
+            Event entity = repository.getOne(id);
+            entity.setDate(dto.getDate());
+            entity.setName(dto.getName());
+            entity.setUrl(dto.getUrl());
+            entity.setCity(new City(dto.getCityId(), null));
+
+            return new EventDTO(entity);
+
+        }catch (EntityNotFoundException e) {
+            throw new EntityNotFoundException("id Not found id: " + id);
+        }
+
+    }
+
+}
